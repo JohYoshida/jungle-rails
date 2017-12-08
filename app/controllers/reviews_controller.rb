fafdf9bbcd35b5aa3441281e6c01682601c25a85
Joh Yoshida <johyoshida@gmail.com>
2017-12-08 15:32:30 -0800

    Require login

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,21 +1,22 @@
 class ReviewsController < ApplicationController
+  before_filter :require_login
 
-def create
-  @review = Review.new(review_params)
-  @product = Product.find(params[:product_id])
-  @review.product = @product
-  @review.user = current_user
+  def create
+    @review = Review.new(review_params)
+    @product = Product.find(params[:product_id])
+    @review.product = @product
+    @review.user = current_user
 
-  if @review.save!
-    redirect_to @product, notice: 'Review was successfully created'
-  else
-    render @product, notice: 'Review was not successfully created'
+    if @review.save!
+      redirect_to @product, notice: 'Review was successfully created'
+    else
+      render @product, notice: 'Review was not successfully created'
+    end
   end
-end
 
-private
+  private
+  
   def review_params
     params.require(:review).permit(:description, :rating)
   end
-
 end
