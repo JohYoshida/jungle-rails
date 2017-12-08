9176770b5e192a4961815f06168ad26734e84e4e
Joh Yoshida <johyoshida@gmail.com>
2017-12-08 12:03:17 -0800

    Changed reviews.rating to integer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171208190800) do
+ActiveRecord::Schema.define(version: 20171208200153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 20171208190800) do
     t.integer  "product_id"
     t.integer  "user_id"
     t.text     "description"
-    t.string   "rating"
+    t.integer  "rating"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
