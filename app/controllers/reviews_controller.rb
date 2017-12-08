40020f5fe095a14e8989e5213b44de4712d299ec
Joh Yoshida <johyoshida@gmail.com>
2017-12-08 15:02:04 -0800

    On POST, save new review

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,21 @@
+class ReviewsController < ApplicationController
+
+def create
+  @review = Review.new(review_params)
+  @product = Product.find(params[:product_id])
+  @review.product = @product
+  @review.user = current_user
+
+  if @review.save!
+    redirect_to @product, notice: 'Review was successfully created'
+  else
+    render @product, notice: 'Review was not successfully created'
+  end
+end
+
+private
+  def review_params
+    params.require(:review).permit(:description, :rating)
+  end
+
+end
