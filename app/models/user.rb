237fa231af2e37fa0ff19703afa13b388ef570f7
Joh Yoshida <johyoshida@gmail.com>
2017-12-08 16:36:16 -0800

    User validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   has_many :reviews
   has_secure_password
+
+  validates :first_name, :last_name, :password_digest, presence: true
+  validates :email, presence: true, uniqueness: true
 end
