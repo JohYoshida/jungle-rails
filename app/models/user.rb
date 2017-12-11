b34f3ab247cd61a1663ac128d2a72f6096600181
Joh Yoshida <johyoshida@gmail.com>
2017-12-11 13:36:34 -0800

    Validate when password or passworf_confirmation are empty should be invalid

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_many :reviews
   has_secure_password
 
+  validates :password, length: { minimum: 1 }
   validates :first_name, :last_name, :password_digest, :password_confirmation, presence: true
   validates :email, presence: true, uniqueness: true
 end
