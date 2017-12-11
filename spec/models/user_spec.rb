7cdcd704cb723b5ad3ef2338f6982a589b456437
Joh Yoshida <johyoshida@gmail.com>
2017-12-11 12:20:06 -0800

    Validate when password and password_confirmation are different should be invalid

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -74,5 +74,18 @@ RSpec.describe User, type: :model do
         expect(@user.valid?).not_to be_truthy
       end
     end
+
+    context 'when password and password_confirmation are different' do
+      it 'should be invalid' do
+        @user = User.new(
+          first_name: 'first',
+          last_name: 'last',
+          email: 'email',
+          password: 'password',
+          password_confirmation: 'not_password'
+        )
+        expect(@user.valid?).not_to be_truthy
+      end
+    end
   end
 end
